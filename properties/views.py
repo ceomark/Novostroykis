ae44995bc42098593a323aad8ef28944a95e8ed0
ceomark <ceomarkplanet@mail.ru>
2024-09-27 15:58:30 +0300

    new migrations

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Property
 
-# Create your views here.
+class PropertyListView(ListView):
+    model = Property
+    template_name = 'properties/property_list.html'
